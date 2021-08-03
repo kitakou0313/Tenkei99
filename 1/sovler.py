910ae8b072bed2c1c128d9f3aa27f2ab31a4daee
kitakou0313 <kitakou0313@gmail.com>
2021-08-03 14:29:52 +0000

    Add ans of problem 1

--- 1/sovler.py
+++ 1/sovler.py
@@ -4,32 +4,32 @@ from typing import List
 最大何個の長さM以上の区間を作れるかを考え、1~Lについて検証
 """
 def isEnableDevideYoukanWithM(A:List[int],K:int ,M:int)->bool:
-    periodLen = 0
+    pre = 0
     periondCnt = 0
     
-    for i in range(len(A)):
-        periodLen += A[i]
-        if periodLen >= M:
-            periodLen = 0
+    for i in range(0, len(A)):
+        if ((A[i] - pre >= M) and (L - A[i] >= M)):
             periondCnt += 1
+            pre = A[i]
     
-    if periondCnt < K:
-        return False
+    if periondCnt >= K:
+        return True
     
-    return True
+    return False
 
 def calMaxYoukanLength(N:int, L:int, K:int, A:List[int]) -> int:
     ng = -1
-    ok = L
+    ok = L+1
 
     while (abs(ok - ng) > 1):
         mid = (ok + ng) // 2
 
         if isEnableDevideYoukanWithM(A, K, mid):
-            ok = mid
-        else:
             ng = mid
-    return ok
+        else:
+            ok = mid
+            
+    return ng
 
 if __name__ == "__main__":
     N, L = map(int, input().split())
