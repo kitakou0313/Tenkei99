6db4ebca2fe8b44ba834d4d43b3a452ff4b59091
kitakou0313 <kitakou0313@gmail.com>
2021-08-04 14:49:35 +0000

    Add templete

---
+++ 2/solver.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def generateAllKakko(N:int) -> List[str]:
+    """
+    docstring
+    """
+    pass
+
+if __name__ == "__main__":
+    N = int(input())
+
+    reses = generateAllKakko(N)
+
+    for res in reses:
+        print(res)
