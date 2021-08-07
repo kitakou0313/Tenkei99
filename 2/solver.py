f8ffecc0b57c0c53b8349d2f3b5e7dd0f422738d
kitakou0313 <kitakou0313@gmail.com>
2021-08-07 08:46:48 +0000

    Add ans of problem 2

--- 2/solver.py
+++ 2/solver.py
@@ -1,16 +1,47 @@
-from typing import List
+from typing import List, Set
 
+def isValid(string:str) -> bool:
+    """
+    正しい括弧文字列か判定
+    """
+    leftKakkoNum = 0
+    rightKakkoNum = 0
+
+    for char in string:
+        if char == "(":
+            leftKakkoNum += 1
+        else:
+            rightKakkoNum += 1
+        if rightKakkoNum > leftKakkoNum:
+            return False
+
+    if not(leftKakkoNum == rightKakkoNum):
+        return False
+    
+    return True
 
 def generateAllKakko(N:int) -> List[str]:
     """
-    括弧の数一つ毎に再帰的につくれば良さそう
+    括弧の数一つ毎に再帰的につくれば良さそう -> ダメ
+    bit全探索でやる
     """
-    pass
+    # 奇数長の長さは生成できないので
+    if N % 2 == 1:
+        return []
+    
+    resSet:List[str] = []
+    for bit in range(0, 2**N):
+        trgString = "".join(["(" if (bit >> ind) & 1 else ")" for ind in range(0, N)])
+        if isValid(trgString):
+            resSet.append(trgString)
+
+    return resSet
 
 if __name__ == "__main__":
     N = int(input())
-
     reses = generateAllKakko(N)
 
-    for res in reses:
+    sortedRes = sorted(reses)
+
+    for res in sortedRes:
         print(res)
